28f19b2a51b6f9ef28987fdd768690123fcdc7aa
sekh mohamad sahil <sekhmohamadsahil01@gmail.com>
2026-08-12 15:07:00 +0530

    Add problem 04: Largest of Two Numbers
    Created a Python program to find the largest of two numbers using if-elif-else statements.

---
+++ problem04_largest_two.py
@@ -0,0 +1,10 @@
+num1=int(input("enter the number"))
+num2=int(input("enter the number"))
+
+if num1>num2:
+    print("largest number",num1)
+
+elif num2>num1:
+     print("largest number",num2)
+else:
+     print("num1and num2 are equal")
