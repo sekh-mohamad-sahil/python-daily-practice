3466aabd58b2ea2606c1abcecda2ab252d6474ba
sekh mohamad sahil <sekhmohamadsahil01@gmail.com>
2026-08-15 00:15:49 +0530

    Add function to sum two numbers
    Add function to sum two numbers using parameters and return the result.

---
+++ functions/function02_add.py
@@ -0,0 +1,10 @@
+# fuction add two numbers.
+def add(a, b):
+    return a + b
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Sum =", add(a, b))
+
+add()
